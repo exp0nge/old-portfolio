978f16d661562c2a2636a3b56852389ddf45aa22
MD R. Islam <mdislamwork@gmail.com>
2015-07-24 17:41:36 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Portfolio rewritten in angular.
---
+++ app.js
@@ -0,0 +1,11 @@
+var portfolioApp = angular.module('portfolioApp', []);
+
+portfolioApp.controller('MainController', ['$http', 
+    function($http){
+        var mv = this;
+        mv.data = [];
+        $http.get('data.json').success(function(response) {
+            mv.data = response;
+            console.log(mv.data)
+        });
+}]);
